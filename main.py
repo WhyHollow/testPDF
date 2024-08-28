1a6ccae8c96cb19954dc646e8d53e0e864b341b3
Hollow <Hollow666Metal@gmail.com>
2024-08-28 23:05:47 +0300

    text

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ async def send_email(user_id: str, subj: str, body: str, files: List[Path]):
     }
 
     payload = {
-        "from": "Platogram <onboarding@resend.dev>",
+        "from": "Welcome Temporary Name",
         "to": user_id,
         "subject": subj,
         "text": body,
